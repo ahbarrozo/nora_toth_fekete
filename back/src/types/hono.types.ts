e08f261708cdde854326799c8507460b62d44835
ahbarrozo <barrozo@tutanota.com>
2025-06-03 15:00:36 +0200

    [v0.2] Update types

--- back/src/types/hono.types.ts
+++ back/src/types/hono.types.ts
@@ -1,7 +1,39 @@
 import { Pool } from 'pg';
-import { User } from './User.type';
+import { Session } from "hono-sessions";
 
-export interface AppVariables {
+export type SessionData = {
+	user?: {
+		id: string;
+		name?: string;
+		email?: string;
+		picture?: string;
+		accessToken?: string;
+	};
+};
+
+export type GoogleOAuthToken = {
+	token: string;
+	expires_in: number;
+	scope: string;
+	token_type: string;
+	id_token?: string;
+	refresh_token?: string;
+};
+
+export type GoogleUserProfile = {
+	id: string;
+	email?: string;
+	verified_email?: boolean;
+	name?: string;
+	given_name?: string;
+	family_name?: string;
+	picture?: string;
+	locale?: string;
+};
+
+export type AppVariables = {
 	db: Pool;
-	user?: User;
-}
+	session: Session<SessionData>;
+	token?: GoogleOAuthToken;
+	['user-google']?: GoogleUserProfile;
+};
