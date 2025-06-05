a8de6654255e745d8d867fc9914e272c4a689f0f
ahbarrozo <barrozo@tutanota.com>
2025-06-05 10:38:02 +0200

    [v0.2] Remove deprecated authentication form

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -11,23 +11,6 @@
 	let authForm = $state({ username: '', password: '' });
 	let modal: HTMLDialogElement;
 
-	async function authenticate() {
-		const authFormData = new FormData();
-		authFormData.append('username', authForm.username);
-		authFormData.append('password', authForm.password);
-
-		const response = await fetch('?/authenticate', {
-			method: 'POST',
-			body: authFormData
-		});
-		const responseData = await response.json();
-
-		if (responseData.status === 200) {
-			modal.close();
-			goto('/admin');
-		}
-	}
-
 	/**
 	 *  Attempts to access an authrized Google account, redirecting
 	 *  to the admin dashboard if successful.
@@ -49,36 +32,6 @@
 		>
 			© Copyright Nora Toth-Fekete - All rights reserved
 		</footer>
-		<dialog bind:this={modal} class="modal">
-			<div class="modal-box w-96">
-				<h3 class="text-lg font-bold">Acesso à página de administração</h3>
-				<fieldset class="fieldset bg-base-200 rounded-box p-4">
-					<label for="title" class="input w-auto text-xl">
-						<input
-							type="input"
-							class="input input-lg"
-							placeholder="Usuário"
-							bind:value={authForm.username}
-						/>
-					</label>
-					<label for="subtitle" class="input w-auto text-xl">
-						<input
-							type="password"
-							class="input input-lg"
-							placeholder="Senha"
-							bind:value={authForm.password}
-						/>
-					</label>
-					<div class="modal-action w-full">
-						<button class="btn btn-primary text-white" onclick={authenticate}>Acessar</button>
-					</div>
-				</fieldset>
-			</div>
-			<form method="dialog" class="modal-backdrop">
-				<button aria-label="Fechar">close</button>
-			</form>
-		</dialog>
-
 		<Toaster />
 	</div>
 </div>
