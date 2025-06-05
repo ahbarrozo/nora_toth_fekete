d80bc3ba9135f2db1248d9962a91c1a05ee06dba
ahbarrozo <barrozo@tutanota.com>
2025-06-05 10:36:56 +0200

    [v0.2] Create constants for type labels and coloring badges

--- front/src/common/constants.ts
+++ front/src/common/constants.ts
@@ -1,3 +1,31 @@
+export const blogPostColors = new Map<string | null, string>(
+    [
+        ['events', 'oklch(45% 0.187 3.815)'],
+        ['music_theory', 'oklch(68% 0.169 237.323)'],
+        ['personal', 'oklch(79% 0.184 86.047)'],
+        [null, 'oklch(27% 0.006 286.033)']
+    ]
+);
+
+export const blogPostTypes = [
+    {
+        displayName: 'Events',
+        name: 'events',
+    },
+    {
+        displayName: 'Music Theory',
+        name: 'music_theory',
+    },
+    {
+        displayName: 'Other',
+        name: null,
+    },
+    {
+        displayName: 'Personal',
+        name: 'personal',
+    }
+];
+
 export const eventTypes = [
     {
         displayName: 'Class',
